85e54565bee0d395a1d23d1162e8b51e1237d4ac
jae <s10205286@connect.np.edu.sg>
2021-11-17 16:49:08 +0800

    Add AccountForm Model and Controller

---
+++ PFD_OCBC_Group5/Models/AccountFormModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFD_OCBC_Group5.Models
+{
+    public class AccountFormModel
+    {
+        [Required]
+        [StringLength(9)]
+        public string NRIC { get; set; }
+
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Nationality { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [StringLength(100)]
+        public string Occupation { get; set; }
+
+        [Display(Name = "Permenant Resident")]
+        public string PR { get; set; }
+
+        public string Gender { get; set; }
+
+        [Display(Name = "Self Employed")]
+        public string SelfEmployed { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Nature of Business")]
+        public string NatureOfBusiness { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Home Address")]
+        public string HomeAddress { get; set; }
+
+        [StringLength(6)]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Mailing Address")]
+        public string MailingAddress { get; set; }
+
+        [StringLength(6)]
+        [Display(Name = "MailingPostalCode")]
+        public string MailingPostalCode { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Mobile Number")]
+        public string MobileNumber { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Home Number")]
+        public string HomeNumber { get; set; }
+
+        public string AccountCreated { get; set; }
+
+    }
+}
