a398158c3a52983b86d52aca45e7403ecb6a6edc
julianee-jk <s10202587@connect.np.edu.sg>
2021-11-18 17:50:19 +0800

    Add NSPVerification Model

---
+++ PFD_OCBC_Group5/Models/NSPVerification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PFD_OCBC_Group5.Models
+{
+    public class NSPVerification
+    {
+        public string NRIC { get; set; }
+
+        public string MobileNo { get; set; }
+
+        public string VerificationImage { get; set; }
+
+        public string VerificationCode { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
