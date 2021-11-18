5d1b1d1cfe2d80c6ddf6971199ed413f579e5e60
ItsNotYJ <yanjieho@gmail.com>
2021-11-18 12:54:04 +0800

    Add JointAccount Model, DAL and Controller

---
+++ PFD_OCBC_Group5/Controllers/JointAccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_OCBC_Group5.Controllers
+{
+    public class JointAccountController : Controller
+    {
+        // GET: JointAccountController
+        // Auto-create account in DAL and retrieve auto-incremented PK as Account Number
+        // Retrieve OwnerNRIC, JointNRIC and Relationship through TempData passing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PFD_OCBC_Group5/Models/JointAccountModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFD_OCBC_Group5.Models
+{
+    public class JointAccountModel
+    {
+        [Display(Name = "Account Number")]
+        public string AccountNumber { get; set; }
+
+        [Display(Name = "Main Account Holder's NRIC")]
+        public string OwnerNRIC { get; set; }
+
+        [Display(Name = "Joint Account Holder's NRIC")]
+        public string JointNRIC { get; set; }
+
+        public string RelationshipToOwner { get; set; }
+    }
+}
