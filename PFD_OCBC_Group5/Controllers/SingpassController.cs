56fc2830bf8b90b62a3c7e5640da4b8e9bc08803
DaveTanKY <73450775+DaveTanKY@users.noreply.github.com>
2022-01-24 00:11:33 +0800

    added firebase access. Added function to retrieve accountholder and user informaiton. Added function to check user login. added function to pass in accountHolder object to form based on returned information from firebase

--- PFD_OCBC_Group5/Controllers/SingpassController.cs
+++ PFD_OCBC_Group5/Controllers/SingpassController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Http;
 using PFD_OCBC_Group5.Models;
 using PFD_OCBC_Group5.DAL;
 using PFD_OCBC_Group5.Extensions;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Diagnostics;
 
 namespace PFD_OCBC_Group5.Controllers
 {
@@ -19,27 +22,201 @@ namespace PFD_OCBC_Group5.Controllers
             return View();
         }
 
-        public ActionResult SingpassLogin()
+        public async Task<ActionResult> SingpassLogin()
         {
+            var accountHolderList = new List<AccountFormModel>();
+            var singpassUserlist = new List<SingpassModel>();
+
+
+
+            DateTime test;
+            test = new DateTime(1990, 11, 17);
+            var firebaseClient = new FirebaseClient("https://pfd-group-5-default-rtdb.firebaseio.com/");
+
+            /*
+            var currentAccountHolder = new AccountFormModel() { AccountID = 1, NRIC = "S1234567C", Name = "Duby" , Salutation = "Miss", Nationality = "Singaporean", DOB = test, Occupation = "Restaurant Owner", PR = "N", Gender = "F", SelfEmployed = "Y", NatureOfBusiness = "Manages a restaurant", HomeAddress = "Clementi Road 54", PostalCode = "583957", MailingAddress = "31 Lifebouy Road" , MailingPostalCode = "561292", Email = "Duby@gmail.com", MobileNumber = "98708394", HomeNumber = "False", AccountCreated = "N" };
+            
+            var result = await firebaseClient
+              .Child("AccountHolder")
+              .PostAsync(currentAccountHolder);
+
+            var singpassUser = new SingpassModel() { NRIC = "S1234567C", Password = "Password123.", DOB = test, Email = "Duby@gmail.com", Gender = "F", HomeAddress = "Clementi Road 54", PostalCode = "583957", MobileNumber = "98708394", Name = "Duby", Nationality = "Singaporean" };
+
+            var result2 = await firebaseClient
+              .Child("SingpassUser")
+              .PostAsync(singpassUser);
+             */
+
+            var singpassAccounts = await firebaseClient
+              .Child("SingpassUser")
+              .OnceAsync<SingpassModel>();
+           
+            foreach (var y in singpassAccounts)
+            {
+                var singpass = y.Object;
+                SingpassModel tempSingpass = new SingpassModel();
+                tempSingpass.DOB = singpass.DOB;
+                tempSingpass.Email = singpass.Email;
+                tempSingpass.Gender = singpass.Gender;
+                tempSingpass.HomeAddress = singpass.HomeAddress;
+                tempSingpass.MobileNumber = singpass.MobileNumber;
+                tempSingpass.NRIC = singpass.NRIC;
+                tempSingpass.Name = singpass.Name;
+                tempSingpass.Nationality = singpass.Nationality;
+                tempSingpass.Password = singpass.Password;
+                tempSingpass.PostalCode = singpass.PostalCode;
+
+                singpassUserlist.Add(tempSingpass);
+
+            }
+
+
+            var accountHolders = await firebaseClient
+              .Child("AccountHolder")
+              .OnceAsync<AccountFormModel>();
+
+
+            foreach (var x in accountHolders)
+            {
+                var account = x.Object;
+                AccountFormModel temp = new AccountFormModel();
+
+                temp.AccountID = account.AccountID;
+
+                
+
+                temp.NRIC = account.NRIC == "False" ?  "" : account.NRIC;
+
+                temp.Name = account.Name == "False" ? "" : account.Name;
+
+                temp.Salutation = account.Salutation == "False" ? "" : account.Salutation;
+
+                temp.Nationality = account.Nationality == "False" ? "" : account.Nationality;
+
+                string g = account.DOB.ToString();
+                temp.DOB = g == "False" ? DateTime.Parse("") : account.DOB;
+
+                temp.Occupation = account.Occupation == "False" ? "" : account.Occupation;
+
+                temp.PR = account.PR == "False" ? "" : account.PR;
+
+                temp.Gender = account.Gender == "False" ? "" : account.Gender;
+
+                temp.SelfEmployed = account.SelfEmployed == "False" ? "" : account.SelfEmployed;
+
+                temp.NatureOfBusiness = account.NatureOfBusiness == "False" ? "" : account.NatureOfBusiness;
+
+                temp.HomeAddress = account.HomeAddress == "False" ? "" : account.HomeAddress;
+
+                temp.PostalCode = account.PostalCode == "False" ? "" : account.PostalCode;
+
+                temp.MailingAddress = account.MailingAddress == "False" ? "" : account.MailingAddress;
+
+                temp.MailingPostalCode = account.MailingPostalCode == "False" ? "" : account.MailingPostalCode;
+
+                temp.Email = account.Email == "False" ? "" : account.Email;
+
+                temp.MobileNumber = account.MobileNumber == "False" ? "" : account.MobileNumber;
+
+                temp.HomeNumber = account.HomeNumber == "False" ? "" : account.HomeNumber;
+
+                temp.AccountCreated = account.AccountCreated == "False" ? "" : account.AccountCreated;
+
+                accountHolderList.Add(temp);
+            }
+
+            TempData.Put("accountHolderList", accountHolderList);
+            TempData.Put("singpassUserList", singpassUserlist);
+            
+           
+
             HttpContext.Session.SetString("Type", "Singpass");
             return View();
         }
 
         [HttpPost]
-        public ActionResult SingpassLogin(string nric)
+        public ActionResult SingpassLogin(string nric, string password)
         {
-            if (AccountContext.AccountExists(nric))
-            {
-                AccountFormModel account = AccountContext.GetApplicantInfo(nric);
-                TempData.Put("firstUserAcc", account);
 
-                // Redirect to form if Singpass account exists
-                return RedirectToAction("PersonInfo", "AccountForm");
+            List<AccountFormModel> accountHolderList = TempData.Get<List<AccountFormModel>>("accountHolderList");
+            List<SingpassModel> singpassUserList = TempData.Get<List<SingpassModel>>("singpassUserlist");
+
+            SingpassModel singpassInfo = new SingpassModel();
+
+            var userExists = false;
+            var passwordCorrect = false;
+
+            foreach(var singpassUser in singpassUserList)
+            {
+                if(singpassUser.NRIC == nric)
+                {
+                    Debug.WriteLine(nric);
+                    Debug.WriteLine(singpassUser.NRIC);
+                    //checking if user exists
+                    userExists = true;
+                    
+
+                    Debug.WriteLine(singpassUser.Password);
+                    if (singpassUser.Password == password)
+                    {
+                        singpassInfo = singpassUser;
+                        //checking if password is correct
+                        passwordCorrect = true;
+                    }
+                    break;
+                }
+            }
+
+            var existingForm = false;
+            if (passwordCorrect)
+            {
+                AccountFormModel account = new AccountFormModel();
+                
+                foreach (var accounts in accountHolderList)
+                {
+                    if (accounts.NRIC == nric)
+                    {
+                        if(accounts.AccountCreated == "N")
+                        {
+                            account = accounts;
+                            //checking if the nric has an existing form that was saved.
+                            existingForm = true;
+                            break;
+                        }
+                    }
+                }
+
+
+                if(existingForm)
+                {
+                    TempData.Put("firstUserAcc", account);
+
+                    // Redirect to form if Singpass account exists
+                    return RedirectToAction("PersonInfo", "AccountForm");
+                }
+                else
+                {
+                    AccountFormModel newAccount = new AccountFormModel();
+                    newAccount.DOB = singpassInfo.DOB;
+                    newAccount.Email = singpassInfo.Email;
+                    newAccount.Gender = singpassInfo.Gender;
+                    newAccount.HomeAddress = singpassInfo.HomeAddress;
+                    newAccount.MobileNumber = singpassInfo.MobileNumber;
+                    newAccount.NRIC = singpassInfo.NRIC;
+                    newAccount.Name = singpassInfo.Name;
+                    newAccount.Nationality = singpassInfo.Nationality;
+                    newAccount.PostalCode = singpassInfo.PostalCode;
+
+                    TempData.Put("firstUserAcc", newAccount);
+                    return RedirectToAction("PersonInfo", "AccountForm");
+                }
             }
             else
             {
                 return RedirectToAction("Index", "Home");
             }
+            
+
         }
     }
 }
