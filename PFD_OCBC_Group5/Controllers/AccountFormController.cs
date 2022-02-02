087e5eb8dae931d07ba41611d47a4308513fe7e7
ItsNotYJ <yanjieho@gmail.com>
2022-02-03 00:18:14 +0800

    Update Firebase DAL code

--- PFD_OCBC_Group5/Controllers/AccountFormController.cs
+++ PFD_OCBC_Group5/Controllers/AccountFormController.cs
@@ -55,48 +55,6 @@ namespace PFD_OCBC_Group5.Controllers
         }
 
         [HttpPost]
-        public ActionResult PersonInfo(string nric,string MobileNo)
-        {
-            Debug.WriteLine("Submit");
-            if(HttpContext.Session.GetString("Status") == "Continue")
-            {
-                string temp;
-                temp = AccountContext.GetNRIC(MobileNo);
-               
-                if(temp != null)
-                {
-                    AccountFormModel account = AccountContext.GetApplicantInfo(temp);
-                    Debug.WriteLine(account.NRIC);
-                    return View(account);
-                }
-            }
-            else if (AccountContext.AccountExists(nric))
-            {
-                AccountFormModel account = AccountContext.GetApplicantInfo(nric);
-                if (account.AccountCreated == "Y")
-                {
-                    return RedirectToAction("SingpassLogin", "Singpass");
-                }
-                else
-                {
-                    return View(account);
-                }
-                
-            }
-            else
-            {
-                AccountFormModel account = new AccountFormModel();
-                account.NRIC = nric;
-                account.DOB = DateTime.Now;
-                return View(account);
-            }
-
-            return RedirectToAction("Index", "Home");
-        }
-
-
-
-        [HttpPost]
         public ActionResult Saveme(AccountFormModel account)
         {
             bool flag = false;
@@ -110,8 +68,10 @@ namespace PFD_OCBC_Group5.Controllers
             client = new FireSharp.FirebaseClient(config);
             FirebaseResponse response = client.Get("AccountHolder");
             dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            
             var list = new List<AccountFormModel>();
             var accexist = false;
+            
             if (data != null)
             {
                 foreach (var item in data)
@@ -128,9 +88,9 @@ namespace PFD_OCBC_Group5.Controllers
                     {
                         accexist = true;
                         account.UniqueID = x.UniqueID;
+                        account.AccountID = x.AccountID;
                         break;
                     }
-
                 }
             }
 
@@ -143,9 +103,11 @@ namespace PFD_OCBC_Group5.Controllers
                 }
                 else
                 {
+                    account.AccountID = list.Count + 1;
                     account.AccountCreated = "N";
                     AddStudentToFirebase(account);
                 }
+
                 /*
                 if (AccountContext.AccountExists(account.NRIC))
                 {
@@ -160,68 +122,21 @@ namespace PFD_OCBC_Group5.Controllers
                     AccountContext.Add(account);
                 }*/
 
-                if (HttpContext.Session.GetString("Type") == "Singpass")
+                if (HttpContext.Session.GetString("Type") == "Singpass" && HttpContext.Session.GetString("Applicant") == "First")
                 {
-                    HttpContext.Session.SetString("FirstNRIC", account.NRIC);
+                    HttpContext.Session.SetInt32("AccountID", account.AccountID);
                     return RedirectToAction("Validate", "SecondEmail");
                 }
+                else if (HttpContext.Session.GetString("Type") == "Singpass" && HttpContext.Session.GetString("Applicant") == "Second")
+                {
+                    HttpContext.Session.SetInt32("SecondUserAccID", account.AccountID);
+                    return RedirectToAction("Index", "JointAccount");
+                }
                 else
                 {
-                    HttpContext.Session.SetString("FirstNRIC", account.NRIC);
+                    HttpContext.Session.SetInt32("AccountID", account.AccountID);
                     return RedirectToAction("UploadPhoto", "NSPVerification");
                 }
-
-            }
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        [HttpPost]
-        public ActionResult Savemesecond(AccountFormModel account, string testing)
-        {
-            bool flag = false;
-            string[] temp = new string[] { };
-
-            temp.Append(account.Occupation);
-            temp.Append(account.PR);
-            temp.Append(account.Gender);
-            temp.Append(account.SelfEmployed);
-            temp.Append(account.HomeAddress);
-            temp.Append(account.PostalCode);
-            temp.Append(account.Email);
-            temp.Append(account.MobileNumber);
-            temp.Append(testing);
-
-            foreach (string x in temp)
-            {
-                if (x == null)
-                {
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag)
-            {
-                if (AccountContext.AccountExists(account.NRIC))
-                {
-                    account.AccountCreated = "N";
-                    AccountContext.Update(account);
-                }
-                else
-                {
-                    //need to change
-                    account.AccountCreated = "N";
-                    AccountContext.Add(account);    
-                }
-
-                JointAccountModel ja = new JointAccountModel();
-                ja.OwnerNRIC = HttpContext.Session.GetString("FirstNRIC");
-                ja.JointNRIC = account.NRIC;
-                ja.RelationshipToOwner = testing;
-                ja.AccountNumber = JointAccountContext.Add(ja);
-                return RedirectToAction("Index", "JointAccount", new { jointAccount = ja});
-
             }
 
             return RedirectToAction("Index", "Home");
@@ -238,8 +153,10 @@ namespace PFD_OCBC_Group5.Controllers
             client = new FireSharp.FirebaseClient(config);
             FirebaseResponse response = client.Get("AccountHolder");
             dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            
             var list = new List<AccountFormModel>();
             var flag = false;
+
             if (data != null)
             {
                 foreach (var item in data)
@@ -256,9 +173,9 @@ namespace PFD_OCBC_Group5.Controllers
                     {
                         flag = true;
                         account.UniqueID = x.UniqueID;
+                        account.AccountID = x.AccountID;
                         break;
                     }
-
                 }
             }
             if (flag)
@@ -268,6 +185,7 @@ namespace PFD_OCBC_Group5.Controllers
             }
             else
             {
+                account.AccountID = list.Count + 1;
                 account.AccountCreated = "N";
                 AddStudentToFirebase(account);
             }
