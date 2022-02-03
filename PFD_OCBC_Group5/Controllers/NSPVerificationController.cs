40b818caeb3d416a5ba48cc21ca4eb1b427a7ef0
julianee-jk <s10202587@connect.np.edu.sg>
2022-02-04 02:17:49 +0800

    Add Verification Code for NSP

--- PFD_OCBC_Group5/Controllers/NSPVerificationController.cs
+++ PFD_OCBC_Group5/Controllers/NSPVerificationController.cs
@@ -8,56 +8,41 @@ using PFD_OCBC_Group5.DAL;
 using PFD_OCBC_Group5.Models;
 using System.IO;
 using System.Diagnostics;
+using System.Drawing;
+using OfficeOpenXml;
 
 namespace PFD_OCBC_Group5.Controllers
 {
     public class NSPVerificationController : Controller
     {
         private NSPVerificationDAL NSPVerificationContext = new NSPVerificationDAL();
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
 
         // GET: NSPVerificationController
         public ActionResult UploadPhoto()
         {
+            string generatedCode = RandomString(8);
+            TempData["generatedCode"] = generatedCode;
             return View();
-        }
-
-        // GET: NSPVerificationController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: NSPVerificationController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: NSPVerificationController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> UploadPhoto(NSPVerification verification, IFormFile photo )
+        public async Task<IActionResult> UploadPhoto(NSPVerification verification, IFormFile photo)
         {
 
             verification.NRIC = HttpContext.Session.GetString("FirstNRIC");
             verification.VerificationDate = DateTime.Now;
 
             HttpContext.Session.SetString("Status", "New");
-            
+
+            CreateExcel(verification.VerificationCode);
 
             if (photo != null &&
             photo.Length > 0)
@@ -82,8 +67,6 @@ namespace PFD_OCBC_Group5.Controllers
                         await photo.CopyToAsync(fileSteam);
                     }
                     verification.VerificationImage = uploadedFile;
-                    
-
 
                     NSPVerificationContext.Add(verification);
 
@@ -99,9 +82,60 @@ namespace PFD_OCBC_Group5.Controllers
                     ViewData["VerificationMessage"] = ex.Message;
                 }
             }
-        
+
 
             return RedirectToAction("Validate", "SecondEmail");
         }
+
+        public ActionResult CreateExcel(string code)
+        {
+            string filepath = "Verification Code Generated.xlsx";
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                //Add Worksheets in Excel file
+                excel.Workbook.Worksheets.Add("Verification Code Generated");
+
+                //Create Excel file in Uploads folder of your project
+                FileInfo excelFile = new FileInfo(filepath);
+
+                //Add header row columns name in string list array
+                var headerRow = new List<string[]>()
+                  {
+                    new string[] { "Verification Code Generated" }
+                  };
+
+                // Get the header range
+                string Range = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+
+                // get the workSheet in which you want to create header
+                var worksheet = excel.Workbook.Worksheets["Verification Code Generated"];
+
+                // Populate & style header row data
+                worksheet.Cells[Range].Style.Font.Bold = true;
+                worksheet.Cells[Range].LoadFromArrays(headerRow);
+
+                //lock all cells except inputs
+                worksheet.Protection.IsProtected = true;
+                worksheet.Cells["A2"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                worksheet.Cells["A2"].Style.Fill.BackgroundColor.SetColor(Color.Yellow);
+
+                //auto size all columns
+                worksheet.Cells["A1:K20"].AutoFitColumns();
+
+                //2 is rowNumber 1 is column number
+                worksheet.Cells[2, 1].Value = code;
+
+                //Save Excel file
+                excel.SaveAs(excelFile);
+            }
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filepath);
+            string fileName = "Verification Code Generated.xlsx";
+
+            //return RedirectToAction("Index");
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+       
     }
 }
