83216fefe54979e5b31fd609ae9e7d7ce564bfea
Timothy Seow <timothyseow18@gmail.com>
2022-02-04 16:16:09 +0800

    add model for verification code from email

---
+++ PFD_OCBC_Group5/Models/VerificationEmail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_OCBC_Group5.Models
+{
+    public class VerificationEmail
+    {
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [Required(ErrorMessage = "Please enter an email address!")]
+        public string EmailAddr { get; set; }
+
+        public string VerificationCode { get; set; }
+
+        public string InputCode { get; set; }
+
+
+    }
+}
